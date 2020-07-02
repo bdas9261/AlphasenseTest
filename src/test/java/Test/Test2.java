02f7ab3ff1fe3adb49f2c752839ff2adffc1e1cf
biswa <bdas9261@gmail.com>
2020-07-02 21:11:52 +0530

    commit added Test Method

--- src/test/java/Test/Test2.java
+++ src/test/java/Test/Test2.java
@@ -1,6 +1,12 @@
 package Test;
 
+import org.junit.Test;
+
 public class Test2 { 
+	@Test
+	public void Test() {
+		System.out.println("Test Method");
+	}
 	public static void main(String[] args) {
 		System.out.println("Test2 class");
 	}
